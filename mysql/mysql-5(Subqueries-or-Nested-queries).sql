2882b74955aeb872da03078b930805f4e03229fb
muhsinmuhsy <muhsinkoodali856@gmail.com>
2024-06-02 22:01:58 +0530

    added-sql

--- mysql/mysql-5(Subqueries-or-Nested-queries).sql
+++ mysql/mysql-5(Subqueries-or-Nested-queries).sql
@@ -2,3 +2,14 @@ CREATE TABLE products(product_id INT, product_name VARCHAR(50), price FLOAT, bra
 CREATE TABLE specifications(ID INT, RAM VARCHAR(50), ROM VARCHAR(50));
 
 
+SELECT RAM, ROM from specifications
+    -> WHERE ID IN (SELECT product_id FROM products WHERE product_name = 'Galaxy A34');
++------+-------+
+| RAM  | ROM   |
++------+-------+
+| 4GB  | 64GB  |
+| 8GB  | 128GB |
+| 16GB | 64GB  |
+| 8GB  | 256GB |
++------+-------+
+4 rows in set (0.01 sec)
---
+++ python/function-1.py
@@ -0,0 +1,7 @@
+def s(*a):
+    for i in a:
+        for j in i:
+            print(i,j)
+
+
+s(1,2,3,4)
